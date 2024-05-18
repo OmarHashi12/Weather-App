b3b75ad756e5f48a0058d1497515152599a3f02b
Omar <hmj9gf@Virginia.edu>
2024-05-18 16:21:41 -0400

    moving around

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import LocationInput from './components/LocationInput';
+import WeatherDisplay from './components/WeatherDisplay';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [coordinates, setCoordinates] = useState(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather App</h1>
+      <LocationInput setCoordinates={setCoordinates} />
+      {coordinates && <WeatherDisplay coordinates={coordinates} />}
     </div>
   );
-}
+};
 
 export default App;
